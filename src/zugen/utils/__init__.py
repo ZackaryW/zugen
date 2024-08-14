63dbdc0299f4cab08f6bdcca3917dd9a1f1865d4
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2024-08-14 14:05:48 -0700

    ref: resolve_path location

--- src/zugen/utils/__init__.py
+++ src/zugen/utils/__init__.py
@@ -1,4 +1,4 @@
-import os
+import os as _os
 from zugen import _shared_folder
 
 def resolve_path(path : str):
@@ -8,11 +8,11 @@ def resolve_path(path : str):
     }
     for k, v in _m.items():
         if path.startswith(v):
-            for o in os.listdir(_shared_folder):
-                if os.path.exists(os.path.join(_shared_folder, o, path[k:])):
-                    return os.path.join(_shared_folder, o, path[k:])
+            for o in _os.listdir(_shared_folder):
+                if _os.path.exists(_os.path.join(_shared_folder, o, path[k:])):
+                    return _os.path.join(_shared_folder, o, path[k:])
             
-    if not os.path.exists(path):
+    if not _os.path.exists(path):
         raise FileNotFoundError(path)
     
     return path
