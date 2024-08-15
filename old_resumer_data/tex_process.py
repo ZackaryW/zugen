b09d83210687ae75d5ef5e5bcade247a46c569b2
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2024-08-14 18:31:49 -0700

    update

--- old_resumer_data/tex_process.py
+++ old_resumer_data/tex_process.py
@@ -1,5 +1,7 @@
 import os
+from zugen.utils import ensure_file
 
+ensure_file("@/old_resumer_data/awesome-cv.cls")
 os.rename("pandoc.out", "resume.tex")
 os.system("xelatex resume.tex")
 print('{"capture": ["resume.pdf"]}')
