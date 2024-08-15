f791f057ad9e7560d168f3c7c21a661fd3468315
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2024-08-14 18:51:17 -0700

    nonstop

--- old_resumer_data/tex_process.py
+++ old_resumer_data/tex_process.py
@@ -3,5 +3,5 @@ from zugen.utils import ensure_file
 
 ensure_file("@/old_resumer_data/awesome-cv.cls")
 os.rename("pandoc.out", "resume.tex")
-os.system("xelatex resume.tex")
+os.system("xelatex -interaction=nonstopmode resume.tex")
 capture("resume.pdf")
